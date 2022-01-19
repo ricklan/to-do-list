0f216aed65ddcfeac8277b639b79e642bb9553e3
Ying Zhou <zhouying@live.ca>
2022-01-19 18:28:30 -0500

    Add basic form in Login component

--- frontend/app/src/Components/Login.js
+++ frontend/app/src/Components/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <>
-      <p>login form</p>
+      <h3>Log In</h3>
+      <form>
+        <div>
+          <label> Username:</label>
+          <input type="text" name="username" id="name" required></input>
+        </div>
+        <div>
+          <label> Passsword:</label>
+          <input type="text" name="password" id="password" required></input>
+        </div>
+        <button>Log In</button>
+      </form>
+      <Link to="/SignUp">Sign Up</Link>
     </>
   );
 }
