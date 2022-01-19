f6f6838bfcc8c1f7cbcb640c1fce98d84a585751
Ying Zhou <zhouying@live.ca>
2022-01-19 18:17:46 -0500

    Create files for home, login and signup

---
+++ frontend/app/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+function Home() {
+  return (
+    <>
+      <Login />
+      <SignUp />
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/app/src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+  return (
+    <>
+      <p>login form</p>
+    </>
+  );
+}
+
+export default Login;
