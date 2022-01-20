431114956b3dcd977d73a502d01faad67184d128
Ying Zhou <zhouying@live.ca>
2022-01-19 19:04:03 -0500

    Add functionality to switch between login and signup components

--- frontend/app/src/Components/Home.js
+++ frontend/app/src/Components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import { BrowserRouter } from "react-router-dom";
 
 function Home() {
   return (
-    <>
-      <Login />
-      <SignUp />
-    </>
+    <BrowserRouter>
+      <div className="login">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+    </BrowserRouter>
   );
 }
 
