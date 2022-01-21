84f2ee9e9322675126c0a3e95ca8e7ed578194bd
Ying Zhou <zhouying@live.ca>
2022-01-21 16:40:26 -0500

    Add functionality to log into dashboard

--- frontend/app/src/Components/Login.js
+++ frontend/app/src/Components/Login.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+const axios = require("axios");
+
+function handleLogin(e, navigate) {
+  e.preventDefault();
+  let username = document.getElementById("name");
+  let pass = document.getElementById("password");
+  let errorTag = document.getElementById("error-login");
+  axios
+    .post("http://127.0.0.1:5000/login", {
+      username: username.value,
+      password: pass.value,
+    })
+    .then(() => {
+      errorTag.innerHTML = "";
+      navigate("/dashboard");
+    })
+    .catch((error) => {
+      if (error.response.status === 404 || error.response.status === 400) {
+        errorTag.innerHTML = "Username or password incorrect";
+      } else {
+        errorTag.innerHTML = error.response.data;
+      }
+    });
+}
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <>
       <h3>Log In</h3>
@@ -12,15 +38,13 @@ function Login() {
         </div>
         <div>
           <label> Passsword:</label>
-          <input type="text" name="password" id="password" required></input>
+          <input type="password" name="password" id="password" required></input>
         </div>
-        <button>
-          <Link to="/dashboard">Log In;</Link>
-        </button>
+        <button onClick={(e) => handleLogin(e, navigate)}>Log In</button>
+        <p id="error-login"></p>
       </form>
       <Link to="/signup">Sign Up</Link>
     </>
   );
 }
-
 export default Login;
