415725250ba3daf7b8734293499309665c8ad352
Ying Zhou <zhouying@live.ca>
2022-02-14 18:37:46 -0500

    Add stylesheet to dashboard

--- frontend/app/src/Components/Dashboard/Dashboard.js
+++ frontend/app/src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import EditTask from "../EditTask/EditTask";
+import "./Dashboard.css";
 
 const axios = require("axios");
 let username;
